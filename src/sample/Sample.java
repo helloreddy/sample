b9dd6d048d02dec17a450bad6d337e03884e4494
fantasticy <iamyoungrok@gmail.com>
2017-04-02 01:30:59 +0900

    added master text

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("branch yr_20170402");
+		System.out.println("master");
 		System.out.println("branch yr_20170402_2");
 	}
 
