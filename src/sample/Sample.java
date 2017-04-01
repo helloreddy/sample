51db1a22c6722bb07f0515e90f9a6964a632f240
fantasticy <iamyoungrok@gmail.com>
2017-04-02 01:31:35 +0900

    added not master text

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -5,6 +5,7 @@ public class Sample {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!");
 		System.out.println("branch yr_20170402");
+		System.out.println("not master");
 		System.out.println("branch yr_20170402_2");
 	}
 
